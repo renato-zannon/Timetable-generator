630dab4359d64ec2b873fcf4f5443c14f6c9d609
Renato Riccieri <renato.riccieri@gmail.com>
2011-02-15 01:52:18 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UFABC::Application.config.secret_token = '80c8f504036b2958d5bb7396aef8126dd7460d1ac73eb4b60d4a4e3b892959ad9c9500fc24387d92fa4b64d55b9eb80a4efedaf7e7a699872a25583886f55a6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UFABC::Application.config.session_store :cookie_store, :key => '_UFABC_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UFABC::Application.config.session_store :active_record_store
