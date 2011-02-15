5131cc0a0453118c9a146eab45622994857df969
Renato Riccieri <renato.riccieri@gmail.com>
2011-02-15 14:27:16 -0200

    Described 'create' action

--- spec/controllers/disciplines_controller_spec.rb
+++ spec/controllers/disciplines_controller_spec.rb
@@ -27,4 +27,20 @@ describe DisciplinesController do
       assigns[:disciplines].should == [@new_discipline]
     end
   end
+  
+  describe "GET 'create'" do
+                      
+    it "instantiates a new discipline with the provided parameters"     
+    it "saves the created discipline"
+    context "when the discipline is saved successfully" do
+      it "sets a 'success' flash message"
+      it "redirects to the disciplines index page"
+    end
+    context "when the discipline fails to save" do
+      it "sets an instance variable with the previous parameters"
+      it "sets an error flash message"
+      it "renders the 'new' page"
+     end
+  end
+  
 end
