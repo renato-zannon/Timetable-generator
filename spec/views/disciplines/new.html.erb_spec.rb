1ba2f2c990df855abacbf75a8b4839e7bf105e50
Renato Riccieri <renato.riccieri@gmail.com>
2011-02-15 11:41:45 -0200

    Described the 'new Discipline' view

---
+++ spec/views/disciplines/new.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'disciplines/new.html.erb' do
+  it "renders a form for the new discipline"
+  it "renders a text field for the discipline name"
+  it "renders a text field for the discipline code"
+  it "renders a text field for the discipline TPI"
+  it "renders a submit button"
+end
