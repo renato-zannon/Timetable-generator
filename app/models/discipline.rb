0d274ede95de88b3e3d5961a7c772bb06b6e544f
Renato Riccieri <renato.riccieri@gmail.com>
2011-02-16 14:10:36 -0200

    Discipline#tpi now green

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,2 +1,24 @@
 class Discipline < ActiveRecord::Base
+  
+  TPI_REGEX = /^(\d{1,2})-(\d{1,2})-(\d{1,2})$/
+  validates :tpi, :format => TPI_REGEX
+  
+  
+  def teoric_hours
+    broken_tpi[0].to_i
+  end
+  
+  def practical_hours
+    broken_tpi[1].to_i
+  end
+  
+  def individual_hours
+    broken_tpi[2].to_i
+  end
+  
+  private
+  
+  def broken_tpi
+    @broken_tpi ||= tpi.scan(TPI_REGEX)[0]
+  end
 end
--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -1,11 +1,39 @@
+#encoding: utf-8
 require 'spec_helper'
 
 describe Discipline do
+  let(:valid_params) do
+     {:name => "Bases Matemáticas",
+      :code => "BC0001",
+      :tpi  => "4-0-6"}
+  end
+  
   describe "#tpi" do
-    it "accepts values on the T-P-I format"
-    it "doesn't accept values not on the T-P-I format"
-    it "sets the #teoric_hours attribute with the T from T-P-I"
-    it "sets the #practical_hours attribute with the P from T-P-I"
-    it "sets the #individual_hours attribute with the I from T-P-I"
+    
+    it "accepts values on the T-P-I format" do
+      discipline = Discipline.new(valid_params)
+      discipline.should be_valid
+    end
+    
+    it "doesn't accept values not on the T-P-I format" do
+      discipline = Discipline.new(valid_params.merge(:tpi => "406"))
+      discipline.should_not be_valid
+    end
+    
+    it "sets the #teoric_hours attribute with the T from T-P-I" do
+      discipline = Discipline.new(valid_params)
+      discipline.teoric_hours.should == 4
+    end
+    
+    it "sets the #practical_hours attribute with the P from T-P-I" do
+      discipline = Discipline.new(valid_params)
+      discipline.practical_hours.should == 0
+    end
+    
+    it "sets the #individual_hours attribute with the I from T-P-I" do
+      discipline = Discipline.new(valid_params)
+      discipline.individual_hours.should == 6
+    end
+    
   end
 end
