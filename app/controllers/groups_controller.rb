066cd64cc58e555f086391f759af25a1b56dffca
Renato Riccieri <renato.riccieri@gmail.com>
2011-02-28 18:14:07 -0300

    Done with the 'Create' action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,19 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.discipline = params[:discipline]
-    @discipline_options = Discipline.all.map { |d| [d.name, d] }
+    @discipline_options = Discipline.all.map { |d| [d.name, d.id] }
+  end
+  
+  def create
+    group = Group.new(params[:group])
+    if group.save == true
+      flash[:notice] = "The group was created successfully!"
+      redirect_to group
+    else
+      flash[:error] = "There was an error while saving the group!"
+      @group = group
+      @discipline_options = Discipline.all.map { |d| [d.name, d.id] }
+      render :action => :new
+    end
   end
 end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe GroupsController do
   
   let(:group) { mock_model('Group').as_new_record.as_null_object }
+  let(:discipline) { double('Discipline') }
   
   describe "GET new" do
     before do
@@ -17,11 +18,62 @@ describe GroupsController do
     
     context "when a discipline is received via params" do
       it "assigns the new group to the discipline" do
-        discipline = double('Discipline')
         group.should_receive('discipline=').with(discipline)
         get :new, :discipline => discipline
       end
     end
   end
   
+  describe "POST create" do
+    let(:group_params) do
+      { 'name'       => "A Diurno",
+        'lessons'    => {'monday' => ["08:00"]},
+        'discipline' => discipline }
+    end
+    
+    before do
+      Group.stub(:new).and_return(group)
+    end
+    
+    it "instantiates a new group with the provided params" do
+      Group.should_receive(:new).with(group_params)
+      post :create, :group => group_params
+    end
+    
+    it "tries to save the group" do
+      group.should_receive(:save)
+      post :create
+    end
+    
+    context "when the group is saved successfully" do
+      before do
+        group.stub(:save).and_return true
+        post :create
+      end
+      
+      it "sets a flash[:notice] message" do
+        flash[:notice].should =~ /success/
+      end
+      
+      it "redirects to the new group's page" do
+        response.should redirect_to group
+      end
+      
+    end
+    context "when the group fails to save" do
+      before do
+        group.stub(:save).and_return false
+        post :create
+      end
+      
+      it "sets a flash[:error] message" do
+        flash[:error].should =~ /error/
+      end
+      
+      it "renders the 'new Group' form" do
+        response.should render_template :new
+      end
+      
+    end
+  end
 end
