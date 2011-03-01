4059534b8ec352a1f535e7637908c277881d1839
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-01 13:04:48 -0300

    Initial draft of the solution

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -4,23 +4,14 @@ module GroupsHelper
 
   include GroupConstants
   
-  def lessons_boxes(f)
-    week_days.each do |day|
-      concat (content_tag :div, :class => "Day", :id => day do
-        concat content_tag :h3, day
-        concat print_lessons_on(day, f)
-      end)
-    end
-  end
-  
-  def print_lessons_on(day, f)
-    f.fields_for day do |day_fields|
-      all_lessons.each do |l|
-        concat(day_fields.label l)
-        concat(day_fields.check_box l)
-        concat(tag :br)
+  def timetable_hash(groups_array)
+    timetable = Hash.new {|hash,key| hash[key] = Hash.new}
+    groups_array.each do |group|
+      group.lessons.each do |day, lessons|
+        lessons.each {|l| timetable[day][l] = group}
       end
     end
+    timetable
   end
   
 end
