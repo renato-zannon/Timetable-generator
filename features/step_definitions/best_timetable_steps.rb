4876b517c0b7e3fec207e979d3419a237e06eefc
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-04 23:30:04 -0300

    Implemented cucumber's final step

--- features/step_definitions/best_timetable_steps.rb
+++ features/step_definitions/best_timetable_steps.rb
@@ -3,3 +3,9 @@ When /^I check "([^"]*)" from the list of disciplines$/ do |code|
   check /#{code}/
   click_button 'Submit'
 end
+
+Then /^I should see a timetable containing the disciplines:$/ do |table|
+ codes = table.raw.flatten
+ codes.each { |code| response.should contain code}
+end
+
